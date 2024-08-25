80195c7f97ca2d9bb8e41a7ae5e77e355a08412e
Daniil Ryabenkov <danilryaba0@gmail.com>
2024-08-25 15:45:48 +0200

    deploy3

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <HashRouter basename='/test_task'>
+    <BrowserRouter basename='/test_task'>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 )
