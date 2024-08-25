81fe701e4a297ba9d861eddc8df822acaf8347f1
Daniil Ryabenkov <danilryaba0@gmail.com>
2024-08-25 13:53:04 +0200

    for deploy2

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>,
+  <Provider store={store}>
+    <HashRouter basename='/test_task/'>
+      <App />
+    </HashRouter>
+  </Provider>
 )
